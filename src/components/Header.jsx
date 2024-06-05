b5890dbad43d3e73655f4553c821a5da016c88a3
ZanellaIgor <igor_zanella@outlook.com>
2024-06-05 00:23:56 -0300

    Alterado CRA para VITE

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import { AppBar, Toolbar } from '@mui/material';
+import React from 'react';
+
+const Header = () => {
+  return (
+    <AppBar>
+      <Toolbar
+        sx={{
+          width: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          flexDirection: 'row',
+        }}
+      >
+        sssadasdasasd
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
