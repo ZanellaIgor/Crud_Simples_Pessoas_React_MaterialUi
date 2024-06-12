f66df078d7c6b0f7478b4805bc2c17c9fc77f10c
ZanellaIgor <igor_zanella@outlook.com>
2024-06-11 22:27:19 -0300

    Finalizado Crud

--- README.md
+++ README.md
@@ -1,8 +1,44 @@
-# React + Vite
+# CRUD de Pessoas com React + Vite
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Projeto desenvolvido para fins de estudo sem utilizar o TypeScript, foi um dos primeiros "projetos" feito para aprender sobre o framework ReactJs, inicialmente foi feito com o Create-React-App, como não é mais recomendado resolvi mudar para o Vite.
 
-Currently, two official plugins are available:
+O sistema, cria, edita e lista pessoas(paginada) pessoas, fazendo validação de campos obigatórios como nome, e cep. Também faz consulta do Cep da pessoa, na api do via cep.
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+A gravação e consulta e gerenciado pelo JSON-Server, que simula uma API real, gravando no arquivo db.json
+
+### Tecnologias/Libs Utilizadas
+
+-ReactJs: Framework JavaScript para desenvolvimento de interfaces de usuário. [https://reactjs.org/]
+-Vite: Ferramenta de desenvolvimento web de alta performance para React. [https://vitejs.dev/]
+-Material UI: Biblioteca de componentes de interface de usuário para React. [https://mui.com/]
+-Yup: Biblioteca para validação de dados. [https://www.npmjs.com/package/yup]
+-React Hook Form: Biblioteca para criação de formulários com hooks. [https://react-hook-form.com/]
+-React Router DOM: Biblioteca para roteamento em React. [https://reactrouter.com/]
+-JSON Server: Servidor mock para desenvolvimento. [https://www.npmjs.com/package/json-server]
+-Via Cep: API para consulta de CEPs. [https://viacep.com.br/]
+
+### Requisitos
+
+-Node.js: Versão 14 ou superior. https://nodejs.org/
+-npm ou yarn: Gerenciador de pacotes.
+
+## Primeiros Passos
+
+1. **Instale as Dependências**: Execute `npm install` ou `yarn` para instalar todas as dependências necessárias.
+
+2. **Construa o Projeto**: Use `npm run build` ou `yarn build` para compilar o projeto.
+
+3. **Roda JSON Server**: Use `json-server --watch db.json` para rodar na porta 3000 o json server.
+
+4. **Inicie o Projeto em Desenvolvimento**: Execute `npm run dev` ou `yarn dev` para iniciar o projeto em desenvolvimento, por padrão na porta 5173.
+
+## Documentação:
+
+JSON SERVER - [https://www.npmjs.com/package/json-server]
+JSON SERVER - [https://github.com/typicode/json-server/tree/v0]
+Via Cep - [https://viacep.com.br/]
+
+## Projeto Finalizado/Descontinuado pelos motivos:
+
+-Objetivo alcançado que era migrar de CRA para Vite.
+-Sem TypeScript não tem o porque fazer um projeto.
--- src/components/TablePessoa/Table.Pessoa.jsx
+++ src/components/TablePessoa/Table.Pessoa.jsx
@@ -10,6 +10,7 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  Typography,
 } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -46,60 +47,70 @@ export const TablePessoa = () => {
   }, [response]);
 
   return (
-    !loading && (
-      <>
-        <TableContainer component={Paper} sx={{ bgcolor: '#E4E4EE' }}>
-          <Table stickyHeader>
-            <TableHead>
-              <TableRow>
-                <TableCell sx={{ bgcolor: '#90AAEF' }}>Nome:</TableCell>
-                <TableCell sx={{ bgcolor: '#90AAEF' }}>Contato</TableCell>
-                <TableCell sx={{ bgcolor: '#90AAEF' }}>Cidade:</TableCell>
-                <TableCell sx={{ bgcolor: '#90AAEF' }}>Bairro:</TableCell>
-                <TableCell sx={{ bgcolor: '#90AAEF' }}></TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {register?.map((reg, index) => {
-                return (
-                  <TableRow
-                    key={reg?.id + index}
-                    sx={{
-                      '&:hover': {
-                        bgcolor: '#9EBDF9',
-                      },
-                    }}
-                  >
-                    <TableCell sx={{ padding: '.8rem' }}>{reg?.nome}</TableCell>
-                    <TableCell sx={{ padding: '.8rem' }}>
-                      {reg?.telefone ?? '' - reg?.celular ?? ''}
-                    </TableCell>
-                    <TableCell sx={{ padding: '.8rem' }}>
-                      {reg?.cidade}
-                    </TableCell>
-                    <TableCell sx={{ padding: '.8rem' }}>
-                      {reg?.bairro}
-                    </TableCell>
-                    <TableCell sx={{ padding: '.8rem' }}>
-                      <IconButton onClick={() => handleClick(reg?.id)}>
-                        <Edit />
-                      </IconButton>
-                    </TableCell>
-                  </TableRow>
-                );
-              })}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <Stack
-          direction="row"
-          justifyContent="center"
-          alignItems="center"
-          sx={{ padding: 2 }}
-        >
-          <Pagination count={pagesTotals} page={page} onChange={handleChange} />
-        </Stack>
-      </>
-    )
+    <>
+      {error && <Typography>Erro na consulta, tente mais tarde!</Typography>}
+      {loading && <Typography>Carregando ... </Typography>}
+      {!loading && !error && (
+        <>
+          <TableContainer component={Paper} sx={{ bgcolor: '#E4E4EE' }}>
+            <Table stickyHeader>
+              <TableHead>
+                <TableRow>
+                  <TableCell sx={{ bgcolor: '#90AAEF' }}>Nome:</TableCell>
+                  <TableCell sx={{ bgcolor: '#90AAEF' }}>Contato</TableCell>
+                  <TableCell sx={{ bgcolor: '#90AAEF' }}>Cidade:</TableCell>
+                  <TableCell sx={{ bgcolor: '#90AAEF' }}>Bairro:</TableCell>
+                  <TableCell sx={{ bgcolor: '#90AAEF' }}></TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {register?.map((reg, index) => {
+                  return (
+                    <TableRow
+                      key={reg?.id + index}
+                      sx={{
+                        '&:hover': {
+                          bgcolor: '#9EBDF9',
+                        },
+                      }}
+                    >
+                      <TableCell sx={{ padding: '.8rem' }}>
+                        {reg?.nome}
+                      </TableCell>
+                      <TableCell sx={{ padding: '.8rem' }}>
+                        {reg?.telefone ?? '' - reg?.celular ?? ''}
+                      </TableCell>
+                      <TableCell sx={{ padding: '.8rem' }}>
+                        {reg?.cidade}
+                      </TableCell>
+                      <TableCell sx={{ padding: '.8rem' }}>
+                        {reg?.bairro}
+                      </TableCell>
+                      <TableCell sx={{ padding: '.8rem' }}>
+                        <IconButton onClick={() => handleClick(reg?.id)}>
+                          <Edit />
+                        </IconButton>
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <Stack
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            sx={{ padding: 2 }}
+          >
+            <Pagination
+              count={pagesTotals}
+              page={page}
+              onChange={handleChange}
+            />
+          </Stack>
+        </>
+      )}
+    </>
   );
 };
